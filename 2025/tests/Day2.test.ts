f4c1c3ee9582a9cd11dc65164dec5f4e9e9143cd
Marshall Beard <maloysiusb@gmail.com>
2025-12-03 12:07:18 -0500

    cleanup

--- 2025/tests/Day2.test.ts
+++ 2025/tests/Day2.test.ts
@@ -1,69 +1,7 @@
 import { describe, expect, test } from 'vitest'
 import { allPartsAreDivisible, isRepeatingCharacter, missMatchId, oddOneOut } from "../Day2";
 
-const allPartsAreDivisiblePairs = [
-    {
-        parts: [56, 56, 56],
-        output: true,
-    },
-    {
-        parts: [56, 65, 56],
-        output: false,
-    },
-    {
-        parts: [96, 96, 96],
-        output: true,
-    },
-    {
-        parts: [969, 696],
-        output: false,
-    },
-]
-
 interface testPairs { input: number, output: number };
-
-const testPairs: testPairs[] = [
-    {
-        input: 10,
-        output: 0,
-    },
-    {
-        input: 11,
-        output: 11,
-    },
-    {
-        input: 12,
-        output: 0,
-    },
-    {
-        input: 4444,
-        output: 4444,
-    },
-    {
-        input: 565656,
-        output: 565656,
-    },
-    {
-        input: 565655,
-        output: 0,
-    },
-    {
-        input: 2233422334,
-        output: 2233422334,
-    },
-    {
-        input: 2233522334,
-        output: 0,
-    },
-    {
-        input: 2222322,
-        output: 0,
-    },
-    {
-        input: 2323232,
-        output: 0,
-    },
-]
 
 describe("Day 2 tests", () => {
     describe("Part B tests", () => {
@@ -142,26 +80,35 @@ describe("Day 2 tests", () => {
         })
 
         test("allPartsAreDivisible evaluates correctly", () => {
+            const allPartsAreDivisiblePairs = [
+                { parts: [56, 56, 56], output: true, },
+                { parts: [56, 65, 56], output: false, },
+                { parts: [96, 96, 96], output: true, },
+                { parts: [969, 696], output: false, },
+            ]
+
             allPartsAreDivisiblePairs.forEach(pair => {
                 expect(allPartsAreDivisible(pair.parts)).toBe(pair.output);
             });
         });
 
         test("missMatchId evaluates correctly", () => {
+            const testPairs: testPairs[] = [
+                { input: 10, output: 0, },
+                { input: 11, output: 11, },
+                { input: 12, output: 0, },
+                { input: 4444, output: 4444, },
+                { input: 565656, output: 565656, },
+                { input: 565655, output: 0, },
+                { input: 2233422334, output: 2233422334, },
+                { input: 2233522334, output: 0, },
+                { input: 2222322, output: 0, },
+                { input: 2323232, output: 0, },
+            ]
+
             testPairs.forEach(pair => {
                 expect(missMatchId(pair.input)).toBe(pair.output);
             });
-        });
-
-        test("missMatchId evaluates correctly", () => {
-            const testCase = `1698522-1698528`;
-            const indexHyphen: number = testCase.indexOf('-');
-            const start: number = Number(testCase.slice(0, indexHyphen));
-            const end: number = Number(testCase.slice(indexHyphen + 1));
-
-            for (let j = start; j <= end; j++) {
-                expect(missMatchId(j)).toBe(0);
-            }
         });
     });
 });
