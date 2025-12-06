6f09d9c2219410432634698f7720cf45ca9b4a11
mbeard <maloysiusb@gmail.com>
2025-12-05 22:01:25 -0500

    Day4B completed

--- 2025/Day4.ts
+++ 2025/Day4.ts
@@ -9,16 +9,12 @@ export const Day4A = (arr: string[]): void => {
             let emptySpace = 0;
             if (i <= 0 || j <= 0 || String(arr[i - 1])[j - 1] === '.') emptySpace++;                         //up and left
             if (i <= 0 || String(arr[i - 1])[j] === '.') emptySpace++;                                       //up and center
-            if (i <= 0 || j >= arrWidth - 1 || String(arr[i - 1])[j + 1] === '.') emptySpace++;                  //up and right
+            if (i <= 0 || j >= arrWidth - 1 || String(arr[i - 1])[j + 1] === '.') emptySpace++;              //up and right
             if (j <= 0 || String(arr[i])[j - 1] === '.') emptySpace++;                                       //left
             if (j >= arrWidth - 1 || String(arr[i])[j + 1] === '.') emptySpace++;                            //right
             if (i >= arr.length - 1 || j <= 0 || String(arr[i + 1])[j - 1] === '.') emptySpace++;            //down and left
             if (i >= arr.length - 1 || String(arr[i + 1])[j] === '.') emptySpace++;                          //down and center
             if (i >= arr.length - 1 || j >= arrWidth - 1 || String(arr[i + 1])[j + 1] === '.') emptySpace++; //down and right
-
-            if (emptySpace > 4) {
-                console.log(`Roll at location [${i}][${j}] has ${emptySpace} empty spaces nearby!`);
-            }
 
             if (emptySpace > 4) total++;
         }
@@ -28,5 +24,32 @@ export const Day4A = (arr: string[]): void => {
 }
 
 export const Day4B = (arr: string[]): void => {
+    let fullArray: string[][] = arr.map(s => s.split(''));
+    let total: number = 0;
+    const arrWidth: number = arr[0]!.length;
 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arrWidth; j++) {
+            if (fullArray[i]![j] !== '@') continue;
+
+            let emptySpace = 0;
+            if (i <= 0 || j <= 0 || fullArray[i - 1]![j - 1] === '.') emptySpace++;                         //up and left
+            if (i <= 0 || fullArray[i - 1]![j] === '.') emptySpace++;                                       //up and center
+            if (i <= 0 || j >= arrWidth - 1 || fullArray[i - 1]![j + 1] === '.') emptySpace++;              //up and right
+            if (j <= 0 || fullArray[i]![j - 1] === '.') emptySpace++;                                       //left
+            if (j >= arrWidth - 1 || fullArray[i]![j + 1] === '.') emptySpace++;                            //right
+            if (i >= arr.length - 1 || j <= 0 || fullArray[i + 1]![j - 1] === '.') emptySpace++;            //down and left
+            if (i >= arr.length - 1 || fullArray[i + 1]![j] === '.') emptySpace++;                          //down and center
+            if (i >= arr.length - 1 || j >= arrWidth - 1 || fullArray[i + 1]![j + 1] === '.') emptySpace++; //down and right
+
+            if (emptySpace > 4) {
+                total++;
+                fullArray[i]![j] = '.';
+                i = 0;
+                j = 0;
+            }
+        }
+    }
+
+    console.log(`Day4B: The total number of paper rolls removed is: ${total}`);
 }
