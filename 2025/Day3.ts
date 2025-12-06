0131e9a09de89d525894725bd710e7317aba999a
Marshall Beard <maloysiusb@gmail.com>
2025-12-05 19:03:52 -0500

    Figured out day 3B in not much more time.

--- 2025/Day3.ts
+++ 2025/Day3.ts
@@ -1,3 +1,5 @@
+import { send } from "process";
+
 export const Day3A = (arr: string[]): void => {
     let total: number = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -7,12 +9,34 @@ export const Day3A = (arr: string[]): void => {
         total += Number(String(start.num) + String(end.num));
     }
 
+    // Answer: 357
     console.log(`Day3A: The total joltage is: ${total}`)
 };
 
 export const Day3B = (arr: string[]): void => {
+    let total: number = 0
+    for (let i = 0; i < arr.length; i++) {
+        const row = arr[i]!;
+        let sectionStart = 0;
+        let sectionEnd = row.length - 11;
 
-    console.log(`Day3B: The total joltage is: ??? `)
+        let currentValue = "";
+
+        while (sectionStart < sectionEnd) {
+            const pair = returnLargestNoPartB(row.slice(sectionStart, sectionEnd));
+            currentValue += pair.num;
+            sectionStart += pair.pos + 1;
+            sectionEnd++;
+
+            if (currentValue.length === 12) {
+                break;
+            }
+        }
+
+        total += Number(currentValue);
+    }
+
+    console.log(`Day3B: The total joltage is: ${total}`)
 };
 
 const returnLargestNo = (str: string, start: boolean): { num: number, pos: number } => {
@@ -32,4 +56,25 @@ const returnLargestNo = (str: string, start: boolean): { num: number, pos: numbe
     }
 
     return { num: maxChar, pos };
+}
+
+const returnLargestNoPartB = (str: string): { num: string, pos: number } => {
+    if (str.length === 1) {
+        return { num: str, pos: 0 };
+    }
+    let maxChar = -1;
+    let pos: number = -1;
+    for (let i = 0; i < str.length; i++) {
+        const num = Number(str[i]);
+        if (num === 9) {
+            return { num: str[i]!, pos: i };
+        }
+
+        if (num > maxChar) {
+            maxChar = num;
+            pos = i;
+        }
+    }
+
+    return { num: String(maxChar), pos };
 }
