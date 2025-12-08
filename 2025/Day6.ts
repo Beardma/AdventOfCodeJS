f3519e0de7e55e10a26165234fd52bb6d8ebd027
Marshall Beard <maloysiusb@gmail.com>
2025-12-08 17:20:07 -0500

    started day 6A

---
+++ 2025/Day6.ts
@@ -0,0 +1,33 @@
+export const Day6A = (input: string[][]): void => {
+    // Transpose this shit because I am lazy
+    const transposeInput = transpose(input);
+    // i 
+
+    let total = 0;
+    for (let i = 0; i < input.length; i++) {
+        const operator = transposeInput[i]![transposeInput[i]!.length - 1];
+        let rowTotal = Number(transposeInput[i]![0]);
+        for (let j = 1; j < transposeInput[i]!.length - 1; j++) {
+            if (operator === '+') {
+                rowTotal += Number(transposeInput[i]![j]);
+            }
+            else {
+                rowTotal = rowTotal * Number(transposeInput[i]![j]);
+            }
+        }
+        total += rowTotal;
+    }
+
+    console.log(`Day6A: The grand total is: ${total}`);
+    // 154917304 => Too low
+}
+
+export const Day6B = (input: string[][]): void => {
+
+}
+
+const transpose = (matrix: string[][]): string[][] => {
+    return matrix[0]!.map((_, colIndex) => {
+        return matrix.map(row => row[colIndex]!);
+    });
+};
--- index.ts
+++ index.ts
@@ -3,24 +3,28 @@ import { Day2A, Day2B } from './2025/Day2';
 import { Day3A, Day3B } from './2025/Day3';
 import { Day4A, Day4B } from './2025/Day4';
 import { Day5A, Day5B } from './2025/Day5';
+import { Day6A, Day6B } from './2025/Day6';
 import { getInputDay1 } from './2025/inputs/Day1inputs'
 import { getInputDay2 } from './2025/inputs/Day2inputs';
 import { getInputDay3 } from './2025/inputs/Day3inputs';
 import { getInputDay4 } from './2025/inputs/Day4inputs';
 import { getInputDay5 } from './2025/inputs/Day5inputs';
+import { getInputDay6 } from './2025/inputs/Day6inputs';
 
 const main = () => {
-    Day1A(getInputDay1());
-    Day1B(getInputDay1());
-    Day2A(getInputDay2());
-    Day2B(getInputDay2());
-    Day3A(getInputDay3());
-    Day3B(getInputDay3());
-    Day4A(getInputDay4());
-    Day4B(getInputDay4());
-    const { ranges, values } = getInputDay5();
-    Day5A(ranges, values);
-    Day5B(ranges);
+    // Day1A(getInputDay1());
+    // Day1B(getInputDay1());
+    // Day2A(getInputDay2());
+    // Day2B(getInputDay2());
+    // Day3A(getInputDay3());
+    // Day3B(getInputDay3());
+    // Day4A(getInputDay4());
+    // Day4B(getInputDay4());
+    // const { ranges, values } = getInputDay5();
+    // Day5A(ranges, values);
+    // Day5B(ranges);
+    Day6A(getInputDay6())
+    Day6B(getInputDay6())
 };
 
 main();
