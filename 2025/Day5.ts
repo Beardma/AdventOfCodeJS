4292160eb323e4f2151c986cadc273c35b55648e
Marshall Beard <maloysiusb@gmail.com>
2025-12-08 10:57:39 -0500

    Day 5B solved

--- 2025/Day5.ts
+++ 2025/Day5.ts
@@ -24,26 +24,63 @@ export const Day5A = (ranges: string[], values: string[]): void => {
     }
 
     console.log(`Day5A: The total number of fresh foods is: ${total}`);
+    // 674
 }
 
 export const Day5B = (ranges: string[]): void => {
-    let freshIds: number[] = [];
 
     const numRanges: Range[] = ranges.map(pair => {
         const [start, end] = pair.split('-');
         return { start: Number(start), end: Number(end) };
     });
 
-    for (let i = 0; i < numRanges.length; i++) {
-        for (let j = numRanges[i]!.start; j <= numRanges[i]!.end; j++) {
-            if (!freshIds.includes(j)) {
-                freshIds.push(j);
+    numRanges.sort((a, b) => a.start - b.start);
+
+    let newRanges: Range[] = [];
+    newRanges.push({ start: numRanges[0]!.start, end: numRanges[0]!.end });
+
+    for (let i = 1; i < numRanges.length; i++) {
+        for (let j = 0; j < newRanges.length; j++) {
+            if (numRanges[i]!.start >= newRanges[j]!.start
+                && numRanges[i]!.end <= newRanges[j]!.end
+            ) {
+                break;
+            }
+            if (numRanges[i]!.start >= newRanges[j]!.start
+                && numRanges[i]!.start <= newRanges[j]!.end + 1
+                && numRanges[i]!.end > newRanges[j]!.end
+            ) {
+                newRanges[j]!.end = numRanges[i]!.end;
+                break;
+            }
+            if (numRanges[i]!.start > newRanges[j]!.end && newRanges[j + 1]) {
+                continue;
+            }
+            else {
+                newRanges.push({ start: numRanges[i]!.start, end: numRanges[i]!.end })
+                break;
             }
         }
-        console.log(`Progress: ${i} / ${numRanges.leng}`)
     }
 
-    console.log(`Day5B: The total number of fresh ingredient ID's is: ${freshIds.length}`);
+    let counter = 1;
+    while (counter < newRanges.length) {
+        if (newRanges[counter - 1]!.end > newRanges[counter]!.start) {
+            newRanges[counter]!.start = newRanges[counter]!.start;
+            newRanges.splice(counter - 1, 1);
+        }
+        else {
+            counter++;
+        }
+    }
+
+    let total = 0;
+    for (let i = 0; i < newRanges.length; i++) {
+        total += newRanges[i]!.end - newRanges[i]!.start + 1;
+    }
+
+    console.log(`Day5B: The total number of fresh ingredient ID's is: ${total}`);
+    // 352509891817881
 }
 
 type Range = {
